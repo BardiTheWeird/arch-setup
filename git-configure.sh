64a5077e1b7580f67730f0c15011e17cef94fb0c
Oleksii Dobrovolskyi <bardianofmandalore@gmail.com>
2022-01-03 11:11:19 +0200

    initial commit

---
+++ git-configure.sh
@@ -0,0 +1,9 @@
+git config --global alias.co checkout
+git config --global alias.st 'status --short'
+git config --global alias.br branch
+git config --global alias.ci commit
+
+git config --global user.name "Oleksii Dobrovolskyi"
+git config --global user.email "bardianofmandalore@gmail.com"
+
+git config --global init.defaultBranch master
