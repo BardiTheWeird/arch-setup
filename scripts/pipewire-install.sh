086b618d20380982e134320076ae3bff3ff37185
Oleksii Dobrovolskyi <bardianofmandalore@gmail.com>
2022-01-04 20:58:47 +0200

    added copying default configs for pipewire

--- scripts/pipewire-install.sh
+++ scripts/pipewire-install.sh
@@ -1,5 +1,9 @@
 sudo pacman -S $(< packages/pipewire)
 
+# Copy default configuration templates
+cp -r /usr/share/pipewire/ /etc/pipewire
+cp -r /usr/share/wireplumber/ /etc/wireplumber
+
 systemctl --user enable --now pipewire.socket
 systemctl --user enable --now pipewire.service
 systemctl --user enable --now wireplumber.service
